b26a106d2c7afe3a6f4f225136d4c36f9b92c3f6
Antonio <35410603+redyz13@users.noreply.github.com>
2023-09-22 20:30:34 +0200

    modified:   nvim/lua/user/plugins/colorscheme.lua 	new file:   nvim/lua/user/plugins/lsp/null-ls.lua 	modified:   nvim/lua/user/plugins/lualine.lua

--- vim/nvim/lua/user/plugins/lualine.lua
+++ vim/nvim/lua/user/plugins/lualine.lua
@@ -1,131 +1,130 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local lualine = require("lualine")
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local lualine = require("lualine")
 
-    local hide_in_width = function()
-      return vim.fn.winwidth(0) > 80
-    end
+		local hide_in_width = function()
+			return vim.fn.winwidth(0) > 80
+		end
 
-    local diagnostics = {
-      "diagnostics",
-      sources = { "nvim_diagnostic" },
-      sections = { "error", "warn" },
-      symbols = { error = " ", warn = " " },
-      colored = false,
-      update_in_insert = false,
-      always_visible = true,
-    }
+		local diagnostics = {
+			"diagnostics",
+			sources = { "nvim_diagnostic" },
+			sections = { "error", "warn" },
+			symbols = { error = " ", warn = " " },
+			colored = false,
+			update_in_insert = false,
+			always_visible = true,
+		}
 
-    local diff = {
-      "diff",
-      colored = false,
-      symbols = { added = " ", modified = " ", removed = " " }, -- 
-      cond = hide_in_width
-    }
+		local diff = {
+			"diff",
+			colored = false,
+			symbols = { added = " ", modified = " ", removed = " " }, --
+			cond = hide_in_width,
+		}
 
-    local mode = {
-      "mode",
-      fmt = function(str)
-        return "-- " .. str .. " --"
-      end,
-    }
+		local mode = {
+			"mode",
+			fmt = function(str)
+				return "-- " .. str .. " --"
+			end,
+		}
 
-    local redyz = {
-      "redyz",
-      fmt = function(str)
-        return "redyz <3"
-      end,
-    }
+		local redyz = {
+			"redyz",
+			fmt = function(str)
+				return "redyz <3"
+			end,
+		}
 
-    local filetype = {
-      "filetype",
-      icons_enabled = true,
-      icon = nil,
-    }
+		local filetype = {
+			"filetype",
+			icons_enabled = true,
+			icon = nil,
+		}
 
-    local branch = {
-      "branch",
-      icons_enabled = true,
-      icon = "",
-    }
+		local branch = {
+			"branch",
+			icons_enabled = true,
+			icon = "",
+		}
 
-    local location = {
-      "location",
-      padding = 0,
-    }
+		local location = {
+			"location",
+			padding = 0,
+		}
 
-    -- Cool function for progress
-    local progress = function()
-      local current_line = vim.fn.line(".")
-      local total_lines = vim.fn.line("$")
-      local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-      local line_ratio = current_line / total_lines
-      local index = math.ceil(line_ratio * #chars)
-      return chars[index]
-    end
+		-- Cool function for progress
+		local progress = function()
+			local current_line = vim.fn.line(".")
+			local total_lines = vim.fn.line("$")
+			local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+			local line_ratio = current_line / total_lines
+			local index = math.ceil(line_ratio * #chars)
+			return chars[index]
+		end
 
-    local spaces = function()
-      return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
-    end
+		local spaces = function()
+			return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+		end
 
-
-    -- Configure lualine with modified theme
-    lualine.setup({
-      options = {
-        globalstatus = false,
-        icons_enabled = true,
-        theme = "catppuccin",
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
-        disabled_filetypes = { "--[[ alpha ]]", "dashboard", "--[[ NvimTree ]]", "Outline" },
-        always_divide_middle = true,
-        -- ignore_focus = {
-        --   "NvimTree",
-        -- },
-      },
-      -- sections = {
-      --   lualine_a = {'mode'},
-      --   lualine_b = {'branch', 'diff', 'diagnostics'},
-      --   lualine_c = {'filename'},
-      --   lualine_x = {'encoding', 'fileformat', 'filetype'},
-      --   lualine_y = {'progress'},
-      --   lualine_z = {'location'}
-      -- },
-      sections = {
-        lualine_a = { branch, diagnostics },
-        lualine_b = { mode },
-        lualine_c = { redyz },
-        -- lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_x = { diff, spaces, "encoding", filetype },
-        lualine_y = { location },
-        lualine_z = { progress },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-      tabline = {},
-      extensions = {},
-      -- options = {
-      --   theme = "catppuccin"
-      -- },
-      -- sections = {
-      --   lualine_x = {
-      --     {
-      --       lazy_status.updates,
-      --       cond = lazy_status.has_updates
-      --     },
-      --     { "encoding" },
-      --     { "fileformat" },
-      --     { "filetype" },
-      --   },
-      -- },
-    })
-  end,
+		-- Configure lualine with modified theme
+		lualine.setup({
+			options = {
+				globalstatus = true,
+				icons_enabled = true,
+				theme = "catppuccin",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+				always_divide_middle = true,
+				ignore_focus = {
+					"NvimTree",
+				},
+			},
+			-- sections = {
+			--   lualine_a = {'mode'},
+			--   lualine_b = {'branch', 'diff', 'diagnostics'},
+			--   lualine_c = {'filename'},
+			--   lualine_x = {'encoding', 'fileformat', 'filetype'},
+			--   lualine_y = {'progress'},
+			--   lualine_z = {'location'}
+			-- },
+			sections = {
+				lualine_a = { branch, diagnostics },
+				lualine_b = { mode },
+				lualine_c = { redyz },
+				-- lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_x = { diff, spaces, "encoding", filetype },
+				lualine_y = { location },
+				lualine_z = { progress },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			extensions = {},
+			-- options = {
+			--   theme = "catppuccin"
+			-- },
+			-- sections = {
+			--   lualine_x = {
+			--     {
+			--       lazy_status.updates,
+			--       cond = lazy_status.has_updates
+			--     },
+			--     { "encoding" },
+			--     { "fileformat" },
+			--     { "filetype" },
+			--   },
+			-- },
+		})
+	end,
 }
