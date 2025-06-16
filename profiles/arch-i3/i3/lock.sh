4115009fbf037157761b705d5b29966599d0e9cb
Antonio <35410603+redyz13@users.noreply.github.com>
2025-06-16 13:01:12 +0200

    modified:   i3/lock.sh

--- profiles/arch-i3/i3/lock.sh
+++ profiles/arch-i3/i3/lock.sh
@@ -1,25 +1,46 @@
 #!/bin/bash
 
-scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 0x8 /tmp/screen_locked_blur.png
+img="/tmp/screen_locked.png"
+blur="/tmp/screen_locked_blur.png"
+
+if scrot "$img"; then
+  convert "$img" -blur 0x6 "$blur"
+else
+  cp "$HOME/.cache/wal/wall.png" "$blur" 2>/dev/null || cp "$HOME/Pictures/wall.jpg" "$blur"
+fi
 
 source "$HOME/.cache/wal/colors.sh"
 
 i3lock \
-  --image=/tmp/screen_locked_blur.png \
-  --inside-color=${color0:1}ff \
-  --ring-color=${color4:1}cc \
+  --time-font="Hack Nerd Font" \
+  --date-font="Hack Nerd Font" \
+  --verif-font="Hack Nerd Font" \
+  --wrong-font="Hack Nerd Font" \
+  --image="$blur" \
+  --blur 5 \
+  --clock \
+  --indicator \
+  --ring-width=8 \
+  --radius=130 \
+  --ring-color=${color4:1}aa \
+  --inside-color=00000088 \
   --line-color=00000000 \
   --separator-color=00000000 \
-  --insidever-color=${color2:1}ff \
+  --keyhl-color=${color5:1}cc \
+  --bshl-color=${color1:1}cc \
+  --insidever-color=00000088 \
   --ringver-color=${color2:1}cc \
-  --insidewrong-color=${color1:1}ff \
+  --insidewrong-color=00000088 \
   --ringwrong-color=${color1:1}cc \
-  --keyhl-color=${color5:1}ff \
-  --bshl-color=${color3:1}ff \
-  --clock \
+  --verif-color=${foreground:1}aa \
+  --wrong-color=${foreground:1}aa \
   --time-color=${foreground:1}ff \
-  --date-color=${foreground:1}ff \
+  --date-color=${foreground:1}cc \
+  --layout-color=${foreground:1}aa \
+  --greeter-color=${foreground:1}aa \
   --time-str="%H:%M:%S" \
-  --date-str="%A, %d %B"
+  --date-str="%A, %d %B" \
+  --noinput-text="" \
+  --wrong-text="" \
+  --verif-text="" \
 
