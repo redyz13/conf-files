1bd8e7f6eedb85ef38fb1406e254df4b37c6fbeb
Antonio <35410603+redyz13@users.noreply.github.com>
2025-06-23 12:00:25 +0200

    modified:   profiles/arch-i3/.xinitrc

--- profiles/arch-i3/.config/polybar/launch.sh
+++ profiles/arch-i3/.config/polybar/launch.sh
@@ -5,13 +5,21 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
 
 if type "xrandr"; then
-	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-	MONITOR=$m polybar --reload bar_top &
-	MONITOR=$m polybar --reload bar_bottom &
-done
+    MON_PRIMARY=$(xrandr --query | grep " connected primary" | cut -d" " -f1)
+    MON_SECONDARY=$(xrandr --query | grep " connected" | grep -v "$MON_PRIMARY" | cut -d" " -f1)
+
+    MONITOR=$MON_PRIMARY polybar --reload bar_top &
+
+    MONITOR=$MON_PRIMARY polybar --reload bar_bottom &
+
+    if [ -n "$MON_SECONDARY" ]; then
+        MONITOR=$MON_SECONDARY polybar --reload bar_top &
+        MONITOR=$MON_SECONDARY polybar --reload bar_bottom &
+    fi
 else
-	polybar --reload bar_top &
-	polybar --reload bar_bottom &
+    polybar --reload bar_top &
+    polybar --reload bar_bottom &
 fi
 
 echo "Polybar launched..."
+
