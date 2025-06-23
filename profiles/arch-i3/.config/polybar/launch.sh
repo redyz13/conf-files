b45583293fb0ef59e98082a1173ea49fd733d9cb
Antonio <35410603+redyz13@users.noreply.github.com>
2025-06-23 10:24:44 +0200

    modified:   profiles/arch-i3/.config/polybar/launch.sh 	modified:   profiles/arch-i3/.xinitrc

--- profiles/arch-i3/.config/polybar/launch.sh
+++ profiles/arch-i3/.config/polybar/launch.sh
@@ -2,7 +2,7 @@
 
 killall -q polybar
 
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
 
 if type "xrandr"; then
 	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
