879c034533b9c4137d4bd9dd670bfc9e6b32cf73
Antonio <35410603+redyz13@users.noreply.github.com>
2025-06-23 12:08:57 +0200

    modified:   polybar/config 	modified:   polybar/launch.sh

--- profiles/arch-i3/.config/polybar/launch.sh
+++ profiles/arch-i3/.config/polybar/launch.sh
@@ -4,6 +4,16 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.2; done
 
+BATTERY=$(ls /sys/class/power_supply/ | grep -E '^BAT' | head -n1)
+ADAPTER=$(ls /sys/class/power_supply/ | grep -E '^(AC|ACAD|ADP)' | head -n1)
+
+# Fallback
+BATTERY=${BATTERY:-BAT1}
+ADAPTER=${ADAPTER:-AC}
+
+export BATTERY
+export ADAPTER
+
 if type "xrandr"; then
     MON_PRIMARY=$(xrandr --query | grep " connected primary" | cut -d" " -f1)
     MON_SECONDARY=$(xrandr --query | grep " connected" | grep -v "$MON_PRIMARY" | cut -d" " -f1)
