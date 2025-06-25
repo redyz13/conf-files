51aa25bfcea18a1e0a2f63b0ad38c7bbc63faaa4
Antonio <35410603+redyz13@users.noreply.github.com>
2025-06-25 10:21:34 +0200

    modified:   polybar/config 	modified:   polybar/ping.sh

--- profiles/arch-i3/.config/polybar/ping.sh
+++ profiles/arch-i3/.config/polybar/ping.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 echo "...   "
 while true; do
-    ping -c 1 -W 1 1.1.1.1 2>/dev/null | awk -F'time=' ' /time=/{print $2}; END{if(NR==0) print "no conn"}'
+    ping -c 1 -W 1 1.1.1.1 2>/dev/null | awk -F'time=' '
+    /time=/ {
+        split($2, a, " ")
+        printf("%d ms\n", a[1])
+    }
+    END {
+        if (NR == 0) print "no conn"
+    }'
     sleep 2
 done
 
