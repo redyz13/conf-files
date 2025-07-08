e62ae7739022c21d8e3bf29ecbd0797642ff7a2c
Antonio <35410603+redyz13@users.noreply.github.com>
2025-07-08 09:38:01 +0200

    modified:   profiles/arch-i3/.config/i3/disable-touchpad-loop.sh

--- profiles/arch-i3/.config/i3/disable-touchpad-loop.sh
+++ profiles/arch-i3/.config/i3/disable-touchpad-loop.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 while true; do
-    ids=$(xinput list | grep -i "Touchpad" | grep -o 'id=[0-9]\+' | cut -d'=' -f2)
+    ids=$(xinput list | grep -E "Wireless Controller Touchpad|Sony Interactive Entertainment Wireless Controller Touchpad" | grep -o 'id=[0-9]*' | cut -d'=' -f2)
     for id in $ids; do
         xinput disable "$id"
     done
     sleep 10
 done
-
 
