25a198c710f29563582b01eb9ee978e7166861f6
Antonio <35410603+redyz13@users.noreply.github.com>
2025-10-03 13:50:40 +0200

    modified:   editor/vim/nvim/lua/user/plugins/lsp/lspconfig.lua

--- editor/vim/nvim/lua/user/plugins/lsp/lspconfig.lua
+++ editor/vim/nvim/lua/user/plugins/lsp/lspconfig.lua
@@ -7,8 +7,6 @@ return {
     { "antosha417/nvim-lsp-file-operations", config = true }
   },
   config = function()
-    local lspconfig = require("lspconfig")
-
     local keymap = vim.keymap
     local opts = { noremap = true, silent = true, buffer = true }
 
@@ -16,12 +14,12 @@ return {
       opts.buffer = bufnr
 
       keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
-      keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts) -- got to declaration
+      keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
       keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
-      keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) -- go to implementation
+      keymap.set("n", "gi", vim.lsp.buf.implementation, opts) -- go to implementation
       keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
       keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
-      keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show  diagnostics for line
+      keymap.set("n", "<leader>D", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show diagnostics for line
       -- keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show diagnostics for cursor
       keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts) -- jump to previous diagnostic in buffer
       keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
@@ -31,29 +29,28 @@ return {
       -- Telescope
       keymap.set("n", "<leader>fr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
       keymap.set("n", "<leader>ft", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-      -- keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+      -- keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show diagnostics for file
     end
 
     local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    lspconfig.pyright.setup {
+    -- LSP servers (nuova API Neovim 0.11+)
+    vim.lsp.config("pyright", {
       on_attach = on_attach,
       capabilities = capabilities,
-    }
+    })
 
-    lspconfig.clangd.setup {
+    vim.lsp.config("clangd", {
       on_attach = on_attach,
       capabilities = capabilities,
+    })
 
-    }
-
-    lspconfig.jdtls.setup {
+    vim.lsp.config("jdtls", {
       on_attach = on_attach,
       capabilities = capabilities,
+    })
 
-    }
-
-    lspconfig.lua_ls.setup {
+    vim.lsp.config("lua_ls", {
       on_attach = on_attach,
       capabilities = capabilities,
       settings = {
@@ -62,7 +59,6 @@ return {
             globals = { "vim" },
           },
           workspace = {
-            -- Make language server aware of runtime files
             library = {
               [vim.fn.expand("$VIMRUNTIME/lua")] = true,
               [vim.fn.stdpath("config") .. "/lua"] = true,
@@ -70,7 +66,7 @@ return {
           }
         }
       }
-    }
+    })
 
     -- Old hint = "ﴞ"
     local signs = { Error = " ", Warn = " ", Hint = "󰵚", Info = " " }
@@ -85,6 +81,7 @@ return {
         },
       },
     })
-
-  end
+  end,
 }
+
+
