b583da8ea2a0c59652399ee70b3160f84d42e1d2
Antonio <35410603+redyz13@users.noreply.github.com>
2025-10-21 14:00:04 +0200

    modified:   profiles/arch-i3/.config/i3/config 	new file:   profiles/arch-i3/.config/i3/power_saving.sh

---
+++ profiles/arch-i3/.config/i3/power_saving.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+STATE_FILE="$HOME/.config/i3/.laptop-mode"
+BRIGHTNESS_LOW=35%
+BRIGHTNESS_HIGH=75%
+
+if [ -f "$STATE_FILE" ]; then
+  echo "[INFO] Disabling power saving mode..."
+  brightnessctl set "$BRIGHTNESS_HIGH"
+  echo "[INFO] Switching TLP to AC mode..."
+  sudo tlp ac
+  notify-send "Power saving disabled"
+  rm "$STATE_FILE"
+else
+  echo "[INFO] Enabling power saving mode..."
+  brightnessctl set "$BRIGHTNESS_LOW"
+  echo "[INFO] Switching TLP to battery mode..."
+  sudo tlp bat
+  notify-send "Power saving enabled"
+  touch "$STATE_FILE"
+fi
+
