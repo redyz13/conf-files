ef05ea69d28977391e84e65b7dd4bb26eb9377ab
Antonio <35410603+redyz13@users.noreply.github.com>
2025-10-27 18:50:06 +0100

    modified:   editor/vim/nvim/lua/user/plugins/pywal16.lua

--- editor/vim/nvim/lua/user/plugins/pywal16.lua
+++ editor/vim/nvim/lua/user/plugins/pywal16.lua
@@ -1,12 +1,22 @@
 return {
   'uZer/pywal16.nvim',
   name = 'pywal',
-  lazy = false, 
+  lazy = false,
   priority = 1001,
   config = function()
     if vim.g.my_colorscheme ~= "pywal16" then return end
     require('pywal16').setup()
     vim.cmd('colorscheme pywal16')
+
+    local function fix_keywords()
+      local kw = vim.api.nvim_get_hl(0, { name = 'Keyword', link = false })
+      if not kw then return end
+      vim.api.nvim_set_hl(0, 'Keyword', vim.tbl_extend('force', kw, { bold = true, cterm = { bold = true } }))
+      vim.api.nvim_set_hl(0, 'Conditional', { link = 'Keyword' })
+      vim.api.nvim_set_hl(0, '@keyword.conditional', { link = 'Keyword' })
+    end
+
+    fix_keywords()
 
     local function apply_style(pattern, style)
       for _, group in ipairs(vim.fn.getcompletion('', 'highlight')) do
@@ -16,12 +26,18 @@ return {
       end
     end
 
-    apply_style('keyword', 'bold')
-    apply_style('statement', 'bold')
-    apply_style('function', 'italic')
-    apply_style('comment', 'italic')
-    apply_style('type', 'italic')
+    apply_style('function',  'italic')
+    apply_style('comment',   'italic')
+    apply_style('type',      'italic')
     apply_style('parameter', 'italic')
+
+    vim.api.nvim_create_autocmd({ 'ColorScheme', 'BufEnter', 'FileType' }, {
+      callback = function()
+        if vim.g.colors_name == 'pywal16' then
+          vim.schedule(fix_keywords)
+        end
+      end
+    })
   end
 }
 
