f14e39f76d5377cfd17af98225ad77a25e8ca2e8
Antonio <35410603+redyz13@users.noreply.github.com>
2025-10-27 16:35:24 +0100

    modified:   editor/vim/nvim/lua/user/plugins/pywal16.lua

--- editor/vim/nvim/lua/user/plugins/pywal16.lua
+++ editor/vim/nvim/lua/user/plugins/pywal16.lua
@@ -7,6 +7,21 @@ return {
     if vim.g.my_colorscheme ~= "pywal16" then return end
     require('pywal16').setup()
     vim.cmd('colorscheme pywal16')
+
+    local function apply_style(pattern, style)
+      for _, group in ipairs(vim.fn.getcompletion('', 'highlight')) do
+        if string.match(string.lower(group), pattern) then
+          vim.cmd(string.format('highlight %s gui=%s cterm=%s', group, style, style))
+        end
+      end
+    end
+
+    apply_style('keyword', 'bold')
+    apply_style('statement', 'bold')
+    apply_style('function', 'italic')
+    apply_style('comment', 'italic')
+    apply_style('type', 'italic')
+    apply_style('parameter', 'italic')
   end
 }
 
