f4b47f5aff9d0421dec7a944664b5184027da8ca
Antonio <35410603+redyz13@users.noreply.github.com>
2026-04-08 00:27:53 +0200

    nvim conf update

--- editor/vim/nvim/lua/user/plugins/comment.lua
+++ editor/vim/nvim/lua/user/plugins/comment.lua
@@ -7,10 +7,17 @@ return {
   config = function()
     local comment = require("Comment")
 
-    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+    local ts_pre_hook =
+      require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()
 
     comment.setup({
-      pre_hook = ts_context_commentstring.create_pre_hook(),
+      pre_hook = function(ctx)
+        local ok, res = pcall(ts_pre_hook, ctx)
+        if ok and res then
+          return res
+        end
+        return vim.bo.commentstring
+      end,
     })
   end,
 }
