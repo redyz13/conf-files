b238ef70a0319b7b6fee0d1b3e6812cbc1e8d4e8
Antonio <35410603+redyz13@users.noreply.github.com>
2026-08-23 13:58:39 +0200

    refactor(polybar): make Spotify updates event-driven

--- profiles/arch-i3/.config/polybar/spotify.py
+++ profiles/arch-i3/.config/polybar/spotify.py
@@ -1,22 +1,73 @@
 #!/usr/bin/env python3
-import dbus
 
-trunclen = 25
+import dbus
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GLib
+
+SERVICE = "org.mpris.MediaPlayer2.spotify"
+PATH = "/org/mpris/MediaPlayer2"
+PLAYER = "org.mpris.MediaPlayer2.Player"
+PROPERTIES = "org.freedesktop.DBus.Properties"
+
+
+def print_metadata(metadata):
+    try:
+        artist = metadata["xesam:artist"][0]
+        title = metadata["xesam:title"]
+        print(f"{artist} - {title}", flush=True)
+    except (KeyError, IndexError, TypeError):
+        print("spotify closed", flush=True)
+
+
+def read_current_metadata():
+    try:
+        obj = bus.get_object(SERVICE, PATH)
+        properties = dbus.Interface(obj, PROPERTIES)
+        metadata = properties.Get(PLAYER, "Metadata")
+        print_metadata(metadata)
+    except dbus.DBusException:
+        print("spotify closed", flush=True)
+
+
+def on_properties_changed(interface, changed, invalidated):
+    if interface != PLAYER:
+        return
+
+    if "Metadata" in changed:
+        print_metadata(changed["Metadata"])
+
+
+def on_name_owner_changed(name, old_owner, new_owner):
+    if name != SERVICE:
+        return
+
+    if new_owner:
+        read_current_metadata()
+    else:
+        print("spotify closed", flush=True)
+
+
+DBusGMainLoop(set_as_default=True)
+
+bus = dbus.SessionBus()
+
+bus.add_signal_receiver(
+    on_properties_changed,
+    signal_name="PropertiesChanged",
+    dbus_interface=PROPERTIES,
+    path=PATH,
+)
+
+bus.add_signal_receiver(
+    on_name_owner_changed,
+    signal_name="NameOwnerChanged",
+    dbus_interface="org.freedesktop.DBus",
+    arg0=SERVICE,
+)
+
+read_current_metadata()
 
 try:
-    session_bus = dbus.SessionBus()
-    spotify_bus = session_bus.get_object(
-        "org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2"
-    )
-
-    spotify_properties = dbus.Interface(spotify_bus, "org.freedesktop.DBus.Properties")
-
-    metadata = spotify_properties.Get("org.mpris.MediaPlayer2.Player", "Metadata")
-
-    artist = metadata["xesam:artist"][0]
-    song = metadata["xesam:title"]
-
-    output = artist + " - " + song
-    print(output)
-except:
-    print("spotify closed")
+    GLib.MainLoop().run()
+except KeyboardInterrupt:
+    pass
